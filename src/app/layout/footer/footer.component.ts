39ff79f904eff0294370183d0fbbf370da31771a
tiago002pro <tiagobarbosa02@outlook.com>
2026-04-02 10:17:13 -0300

    Implementando o footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  currentYear = new Date().getFullYear();
 
+  cidades = [
+    {
+      nome: 'Maringá',
+      endereco: 'Rua Pioneiro José Jacinto Maia, 250 - Jardim Copacabana'
+    },
+    {
+      nome: 'Mandaguaçu',
+      endereco: 'Av. Munhoz da Rocha, 1671 - Centro'
+    }
+  ];
+
+  socialMedia = [
+    { icon: 'bi bi-instagram', link: 'https://www.instagram.com/renascermaringa/?hl=pt-br' },
+    { icon: 'bi bi-facebook', link: 'https://www.facebook.com/igrejarenascermaringa' },
+    { icon: 'bi bi-youtube', link: 'https://www.youtube.com/@RenascerMaringá' },
+  ];
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
+
+  scrollTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
