82f5fd7a40fab50e0a73d914bd2833f3aaa9914a
tiago002pro <tiagobarbosa02@outlook.com>
2026-04-02 01:09:31 -0300

    Criando uma tela sobre generica

--- src/app/pages/home/sections/sobre/sobre.component.ts
+++ src/app/pages/home/sections/sobre/sobre.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RevealDirective } from '../../../../core/directive/reveal.directive';
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [],
+  imports: [RevealDirective],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.scss'
 })
