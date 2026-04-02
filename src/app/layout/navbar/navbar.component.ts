fef414ab374a0453f76174a9bb36a183ee451918
tiago002pro <tiagobarbosa02@outlook.com>
2026-04-02 01:04:45 -0300

    Navbar dinâmica no scroll

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { ScrollSpyService } from '../../core/service/scroll-spy.service';
 import { AsyncPipe } from '@angular/common';
@@ -17,6 +17,19 @@ export class NavbarComponent {
     { label: 'Quem Somos', fragment: 'sobre' },
     { label: 'Onde Estamos', fragment: 'onde-estamos' },
   ]
+
+  isScrolled = false;
+  private lastState = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const current = window.scrollY > 50;
+
+    if (current !== this.lastState) {
+      this.isScrolled = current;
+      this.lastState = current;
+    }
+  }
 
   activeSection$;
 
